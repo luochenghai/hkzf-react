6e1ba85904c91ccb10f80f934052e78862bbc458
罗成海 <1553650095@qq.com>
2021-01-11 11:10:20 +0800

    tabbar路由重定向

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react'
 // 在这个文件写路由代码
 import Home from "./pages/Home/index.js";
 import Map from "./pages/Map/index.js";
-import { BrowserRouter as Router ,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+
+const Auth = () => { 
+  // 在函数体内可以写if else 来控制组件的显示与隐藏
+  return <Redirect from='/' to='/home'/>
+}
 class App extends React.Component { 
   componentDidMount() { }
   render() { 
     return (
-      <Router>
+      <Router>index
         <div className="App">
-            <Route component={Home}></Route>
-            <Route path='/map' component={Map }></Route>
+          <Route path='/home' component={Home}></Route>
+          <Route path='/map' component={Map}></Route>
+          {/* <Redirect from='/' to='/home'/> */}
+          {/* 进一步优化，写成函数组件 */}
+          <Auth/>
        </div>
       </Router>
     )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,14 +7,67 @@ import '../../assets/fonts/iconfont.css'
 import Index from '../../pages/Index/index.js'
 import HouseList from '../../pages/HouseList/index.js'
 import Profile from '../../pages/Profile/index.js'
-    
-    
+import { tabbardata} from './tabbardata.json'
+//常量数据写在类组件的外面（最终应该写在一个json文件中）    
+// const tabbardata = [{
+//     title: '首页',
+//     key: 'index',
+//     iconUrl: 'icon-ind',
+//     path:'/home/index'
+// },
+// {
+//     title: '找房',
+//     key: 'house',
+//     iconUrl: 'icon-findHouse',
+//     path:'/home/house'
+//     },
+//     {
+//         title: '我的',
+//         key: 'profile',
+//         iconUrl: 'icon-my',
+//         path:'/home/profile'
+//     }]
 class Home extends React.Component { 
     componentDidMount() { }
     state = {
-        selectedTab: 'redTab'
+        // selectedTab: 'index'
+        selectedTab: this.props.location.pathname
     };
 
+   renderComponent = (path) => { 
+        switch (path) { 
+            case '/home':
+                return <Index />
+            case '/home/house':
+                return < HouseList/>
+            case '/home/profile':
+                return <Profile />
+             default:
+                return <Index/>
+        }
+    }
+    renderTabBar = () => { 
+        return tabbardata.map(item => { 
+            return   <TabBar.Item
+                title={ item.title}
+                key={ item.key}
+                icon={<i className={ `iconfont ${item.iconUrl}`}/>}
+                selectedIcon={<i className={ `iconfont ${item.iconUrl}`}/>
+                }
+                selected={this.state.selectedTab === item.path}
+                onPress={() => {
+                    this.props.history.push(item.path);
+                    this.setState({
+                    selectedTab: item.path,
+                    });
+                }}                  
+            >
+                {/* 切换显示的组件 */}
+                { this.renderComponent(item.path)}
+            </TabBar.Item>
+
+        })
+    }
     render() { 
         // return <div>
         //     <Route path="/home/index" component={Index}></Route>
@@ -29,26 +82,29 @@ class Home extends React.Component {
         return (
             <div className="Tabbar">
             <TabBar
-                unselectedTintColor="#949494"
-                tintColor="#33A3F4"
-                barTintColor="white"
-                
-              >
-                <TabBar.Item
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                    noRenderContent={ false}
+                >
+                    { this.renderTabBar()}
+
+                {/* <TabBar.Item
                   title="首页"
                   key="Life"
                   icon={<i className="iconfont icon-ind"/>}
                   selectedIcon={<i className="iconfont icon-ind"/>
                   }
-                  selected={this.state.selectedTab === 'redTab'}
+                  selected={this.state.selectedTab === '/home/index'}
                   onPress={() => {
-                    this.setState({
-                      selectedTab: 'redTab',
-                    });
+                      this.props.history.push('/home/index');
+                      this.setState({
+                        selectedTab: '/home/index',
+                      });
                   }}
                  
                 >
-                 
+                 <Index/>
                 </TabBar.Item>
                 <TabBar.Item
                 icon={<i className="iconfont icon-findHouse"/>}
@@ -57,17 +113,18 @@ class Home extends React.Component {
                   }
                   title="找房"
                   key="Koubei"
-                  selected={this.state.selectedTab === 'blueTab'}
+                  selected={this.state.selectedTab === '/home/house'}
                   onPress={() => {
-                    this.setState({
-                      selectedTab: 'blueTab',
-                    });
+                      this.props.history.push('/home/house');
+                      this.setState({
+                        selectedTab: '/home/house',
+                      });
                   }}
                   data-seed="logId1"
                 >
-                
-                </TabBar.Item>
-                <TabBar.Item
+                <HouseList/>
+                </TabBar.Item> */}
+                {/* <TabBar.Item
                   icon={<i className="iconfont icon-ind"/>}
                   selectedIcon={
                     <i className="iconfont icon-ind"/>
@@ -76,27 +133,26 @@ class Home extends React.Component {
                   key="Friend"
                   selected={this.state.selectedTab === 'greenTab'}
                   onPress={() => {
-                    this.setState({
-                      selectedTab: 'greenTab',
-                    });
+                    this.props.history.push('/home/index')
                   }}
                 >
                
-                </TabBar.Item>
-                <TabBar.Item
+                </TabBar.Item> */}
+                {/* <TabBar.Item
                  icon={<i className="iconfont icon-my"/>}
                   selectedIcon={<i className="iconfont icon-my"/>}
                   title="我的"
                   key="my"
-                  selected={this.state.selectedTab === 'yellowTab'}
+                  selected={this.state.selectedTab === '/home/profile'}
                   onPress={() => {
-                    this.setState({
-                      selectedTab: 'yellowTab',
-                    });
+                      this.props.history.push('/home/profile');
+                      this.setState({
+                        selectedTab: '/home/profile',
+                      });
                   }}
                 >
-         
-                </TabBar.Item>
+               <Profile/>
+                </TabBar.Item> */}
               </TabBar>
             </div>
           );
