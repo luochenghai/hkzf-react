a52cb240027020ded104523da8b24ac04f5be228
罗成海 <1553650095@qq.com>
2021-01-12 22:14:24 +0800

    导航nav完成

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import { Carousel } from 'antd-mobile';
-import { getSwiper} from './api.js'
+import { Carousel ,Flex } from 'antd-mobile';
+import { getSwiper } from './api.js'
+import './index.css'
+// withRouter 引入这个高阶组件是为了使index组件导出后的props有值
+import { withRouter } from "react-router-dom";
+// 引入图片
+import  Nav1  from "../../assets/images/nav-1.png";
+import  Nav2  from "../../assets/images/nav-2.png";
+import  Nav3  from "../../assets/images/nav-3.png";
+import  Nav4  from "../../assets/images/nav-4.png";
 // 通过Home 中的三个Link导航切换三个组件的显示与隐藏
 const BaseURL = `http://localhost:8080`
+const nav = [
+  {
+    img: Nav1,
+    title: '整租',
+    path: '/home/list',
+    id:0
+  },
+  {
+    img: Nav2,
+    title: '合租',
+    path:'/home/list',
+    id:1
+  },
+  {
+    img: Nav3,
+    title: '地图找房',
+    path: '/map',
+    id:2
+  },
+  {
+    img: Nav4,
+    title: '去出租',
+    path: '/login',
+    id:3
+  }
+]
 class Index extends React.Component { 
     state = {
       swiperData: [],
@@ -28,8 +62,7 @@ class Index extends React.Component {
             })
          })
         }
-      }
-
+     }
       // 轮播图方法
       renderSwiper = () => { 
         const { swiperData } = this.state;
@@ -51,19 +84,35 @@ class Index extends React.Component {
             />
           </a>
       ))
+  }
+    // 导航
+  renderNav = () =>{
+    // 第一层return 返回的是一个数组
+    return nav.map((item) => { 
+        // 第二层return 返回的是一个react 元素
+      return <Flex.Item className="nav" key={item.id} onClick={() => { 
+        this.props.history.push(item.path)
+      }} >
+                <img src={item.img} alt="图片无法显示" />
+                <p>{ item.title}</p>
+            </Flex.Item>
+      })
     }
   render() { 
     const { loadFinshed } = this.state;
       return <div>
-            {/* 轮播图 */}
-                <Carousel autoplay={loadFinshed} infinite>
-                  { this.renderSwiper()}
-                </Carousel>
+        {/* 轮播图 */}
+            <Carousel autoplay={loadFinshed} infinite>
+              { this.renderSwiper()}
+            </Carousel>
           
-            {/* 宫格菜单 */}
+        {/* 宫格菜单 */}
+        <Flex>
+           {this.renderNav()} 
+        </Flex>
             {/* 租房小组-导航（4个） */}
             {/* 最新资讯 */}
       </div>
     }
 }
-export default Index
+export default withRouter(Index)
