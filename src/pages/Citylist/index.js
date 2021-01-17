ce3f51ad9ef3879de8363e6606a36ccb5339b8a1
罗成海 <1553650095@qq.com>
2021-01-17 22:20:08 +0800

    城市选择完成

--- src/pages/Citylist/index.js
+++ src/pages/Citylist/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { NavBar, Icon } from 'antd-mobile';
+import { NavBar, Icon ,Toast } from 'antd-mobile';
+import {List,AutoSizer} from 'react-virtualized';
 import { getAreaCity ,getHotCity} from '../Index/api.js'
 import { getCurrentCity } from "../../utils/getCurrentCity.js";
-//import Item from 'antd-mobile/lib/popover/Item';
+import { setCity } from "../../utils/lsCity.js";
+
+import './index.css'
 class CityList extends React.Component { 
-    componentDidMount() { this.loadCities()}
-   
-    formCityData = (data)=>{
+    // 状态数据
+    state = {
+       cityIndex:[],
+       citylist:{}
+    }
+ componentDidMount() { this.loadCities()}
+    // react 长列表
+ rowRenderer = ({ key, index, style}) =>{
+      const {cityIndex,citylist} = this.state;
+      const letter = cityIndex[index];
+      return (
+        <div key={key} style={style} className="city">
+          <div className="title"> {letter}</div>
+          {/* 插值表达式里可以放数组 */}
+          {citylist[letter].map(item=>{
+            return  <div className="name" key={item.value} onClick={()=>{
+              // 筛选北京上海广州 深圳，目前这几个城市有房源数据
+              if (['北京','上海','深圳','广州'].includes(item.label)){
+                 this.props.history.push('/home');
+                 setCity({label:item.label,value:item.value});
+              } else {
+                Toast.info('暂无房源', 2, null, false);
+              }
+            }}>{item.label}</div>
+          })}
+         
+        </div>
+      );
+    }
+  formCityData = (data)=>{
       let cityIndex =[];
       let citylist ={};
       data.map(item=>{
@@ -24,7 +54,7 @@ class CityList extends React.Component {
     }
 
 
-    loadCities = async () => { 
+  loadCities = async () => { 
       const { data } = await getAreaCity();
       // 获取热门城市
       const { data:hotData } = await getHotCity();
@@ -34,26 +64,51 @@ class CityList extends React.Component {
         // 处理后台返回的数据
         const {cityIndex,citylist} = this.formCityData(body);
         // 在原来的数据的基础上增加热门城市
-        cityIndex.unshift('hot');
-        citylist['hot'] = hotData.body;
+        cityIndex.unshift('热门城市');
+        citylist['热门城市'] = hotData.body;
         // 当前城市
         getCurrentCity().then(res=>{
           const {label,value} =res;
-          cityIndex.unshift('curCity');
-          citylist['curCity'] = [{label,value}];
+          cityIndex.unshift('当前城市');
+          citylist['当前城市'] = [{label,value}];
+          this.setState(()=>{
+            return {...this.state,cityIndex,citylist}
+          })
         })
-        console.log(citylist,cityIndex)
+       
+      //  console.log(citylist,cityIndex)
       }
      
   }
+  // 计算行高
+  calHeight = ({index})=>{
+    const {cityIndex,citylist} = this.state;
+    const letter = cityIndex[index];
+    return 36+50*citylist[letter].length;
+  }
 
-    render() {
-        return (<div>
+  render() {
+    const {cityIndex} = this.state;
+        return (<div className="citylist">
             <NavBar
-        mode="light"
-        icon={<Icon type="left" />}
-        onLeftClick={() => {this.props.history.goBack()}}
-      >选择城市</NavBar>
+                className="navbar"
+                mode="light"
+                icon={<Icon type="left" />}
+                onLeftClick={() => {this.props.history.goBack()}}
+              >选择城市</NavBar>
+              {/* 长列表 */}
+              {/* AutoSizer 其实就是一HOC 高阶组件 */}
+              <AutoSizer>
+                {({height, width}) => (
+                  <List
+                    width={width}
+                    height={height}
+                    rowCount={cityIndex.length}  
+                    rowHeight={this.calHeight}
+                    rowRenderer={this.rowRenderer}
+                  />
+                  )}
+                 </AutoSizer>
         </div>)
      }
 }
