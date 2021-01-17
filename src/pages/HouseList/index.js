5dfa39c787d20f7d51a714731a1f3d89588b970c
罗成海 <1553650095@qq.com>
2021-01-18 00:50:09 +0800

    找房列表--filterFitle完成

--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import  Filter  from "./components/Filter/index.js";
 // 通过Home 中的三个Link导航切换三个组件的显示与隐藏
 class HouseList extends React.Component { 
     componentDidMount() { }
     render() { 
-        return (<div></div>)
+        return (<div>
+            <Filter/>
+        </div>)
     }
 }
 export default HouseList
